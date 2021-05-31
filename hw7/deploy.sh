0dac9db9331772e58e8879f3d5d5b8b19c09279d
Shamil Gumerov <shamusg12345@gmail.com>
2021-05-31 13:38:54 +1100

    updated deploy.sh

--- hw7/deploy.sh
+++ hw7/deploy.sh
@@ -4,13 +4,19 @@ echo Starting docker containers deployment:;
 docker-compose up -d;
 sleep 5;
 echo Creating source and destination databases:;
-docker exec -it -d my_postgres_1 psql -U root -c "create database my_database_1";
-docker exec -it -d my_postgres_2 psql -U root -c "create database my_database_2";
+docker exec -it my_postgres_1 psql -U root -c "drop database if exists my_database_1;";
+docker exec -it my_postgres_2 psql -U root -c "drop database if exists my_database_2;";
+docker exec -it my_postgres_1 psql -U root -c "create database my_database_1";
+docker exec -it my_postgres_2 psql -U root -c "create database my_database_2";
 docker cp ./dss.ddl my_postgres_1:/;
 docker cp ./dss.ddl my_postgres_2:/;
-docker exec -it -d my_postgres_1 psql my_database_1 -f dss.ddl;
-docker exec -it -d my_postgres_2 psql my_database_2 -f dss.ddl;
-echo Popullating destination database with data;
+docker exec -it my_postgres_1 psql my_database_1 -f dss.ddl;
+docker exec -it my_postgres_2 psql my_database_2 -f dss.ddl;
+echo check:
+docker exec -it my_postgres_1 psql my_database_1 -U root -c "\dt;";
+docker exec -it my_postgres_1 psql my_database_1 -U root -c "\dt;";
+
+echo Popullating source database with data;
 docker cp ./tbl my_postgres_1:/;
 docker exec -it my_postgres_1 psql my_database_1 -c "\copy customer FROM '/tbl/customer.tbl' CSV DELIMITER '|'";
 docker exec -it my_postgres_1 psql my_database_1 -c "\copy lineitem FROM '/tbl/lineitem.tbl' CSV DELIMITER '|'";
